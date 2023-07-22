209ae1d525e223f5432aeb8380219225848f287c
AlexZav1327 <alexey.zarapin@gmail.com>
2023-07-23 00:49:25 +0300

    feat: save and show information about all visits

--- internal/service/service.go
+++ internal/service/service.go
@@ -18,11 +18,11 @@ func (v *VisitInfo) ShowVisitInfo(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Current date and time is: %s\n\n", currentTime)
 	fmt.Fprintf(w, "Your IP address is: %s", r.RemoteAddr)
 
-	v.timeVisit = currentTime
-	v.ipAddress = r.RemoteAddr
+	v.timeVisit += fmt.Sprintf("%s\n", currentTime)
+	v.ipAddress += fmt.Sprintf("%s\n", r.RemoteAddr)
 }
 
 func (v *VisitInfo) ShowPrevVisitInfo(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintf(w, "Previous visit date and time was: %s\n\n", v.timeVisit)
-	fmt.Fprintf(w, "Previous user IP address was: %s", v.ipAddress)
+	fmt.Fprintf(w, "Previous visits date and time were:\n%s\n\n", v.timeVisit)
+	fmt.Fprintf(w, "Previous user's IP addresses were:\n%s", v.ipAddress)
 }
